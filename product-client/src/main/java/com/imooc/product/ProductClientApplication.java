1c593b38e189c9be1dbc71f978fe1d5c744fb48d
ZhengHaiLong <13260541671@163.com>
2018-11-14 21:11:42 +0800

    module

--- product-client/src/main/java/com/imooc/product/ProductClientApplication.java
+++ product-client/src/main/java/com/imooc/product/ProductClientApplication.java
@@ -2,13 +2,11 @@ package com.imooc.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ProductApplication {
+public class ProductClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
+		SpringApplication.run(ProductClientApplication.class, args);
 	}
 }
